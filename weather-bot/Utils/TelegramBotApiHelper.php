2248a635f55cfc8fa085c876154484217cdceae7
Aleksandr Yurchenko <y.aleksandr89@yandex.ru>
2023-07-13 22:51:56 +0300

    weather-bot: get response Open Weather (send name city)

--- weather-bot/Utils/TelegramBotApiHelper.php
+++ weather-bot/Utils/TelegramBotApiHelper.php
@@ -2,16 +2,19 @@
 
 namespace WeatherBot\Utils;
 
+use GuzzleHttp\Exception\GuzzleException;
 use Telegram\Bot\Api as TelegramBotApi;
 use Telegram\Bot\Exceptions\TelegramSDKException;
 use Telegram\Bot\Objects\Message;
 use Telegram\Bot\Objects\Message as MessageObject;
 use Telegram\Bot\Objects\Update;
+use GuzzleHttp\Client;
 
 class TelegramBotApiHelper
 {
     /**
      * @throws TelegramSDKException
+     * @throws GuzzleException
      *
      * // Из-за $telegram->answerCallbackQuery возвращаемый тип MessageObject|bool,
      * а не Telegram\Bot\Objects\Message ( ->answerCallbackQuery - возвращает bool)
@@ -26,11 +29,7 @@ class TelegramBotApiHelper
             'chatId' => $chatId,
             'incomingText' => $incomingText
         ] = self::getDataForWork($update, $nameArrMessage);
-//self::writeToLogs(
-//    [],
-//    __DIR__ . '/../test.log'
-//);
-//die;
+
         if ('/start' === $incomingText) {
             $message = <<< MESSAGE
             Привет!
@@ -43,6 +42,15 @@ class TelegramBotApiHelper
                 telegram: $telegram,
                 chatId: $chatId,
                 message: $message,
+                additionalParams: [
+                    'parse_mode' => 'HTML'
+                ]
+            );
+        } elseif ('' !== $incomingText) {
+            $response = self::sendMessage(
+                telegram: $telegram,
+                chatId: $chatId,
+                message: self::getResponseToOpenWeatherApi('GET', $incomingText),
                 additionalParams: [
                     'parse_mode' => 'HTML'
                 ]
@@ -62,15 +70,13 @@ class TelegramBotApiHelper
     {
         if (count($update) > 0) {
             ob_start();
-            if (array_key_exists('message', $update)) {
-                echo '===[' . date('d-m-Y H:i:s', $update['message']['date']) . ']===' . PHP_EOL;
-            } elseif (array_key_exists('edited_message', $update)) {
-                echo '===[' . date('d-m-Y H:i:s', $update['edited_message']['edit_date']) . ']===' . PHP_EOL;
-            } elseif (array_key_exists('callback_query', $update)) {
-                echo '===[' . date('d-m-Y H:i:s', $update['callback_query']['message']['date']) . ']===' . PHP_EOL;
-            } else {
-                echo '===[' . date('d-m-Y H:i:s', $update['date']) . ']===' . PHP_EOL;
-            }
+            echo match (true) {
+                array_key_exists('message', $update) => '===[' . date('d-m-Y H:i:s', $update['message']['date']) . ']===' . PHP_EOL,
+                array_key_exists('edited_message', $update) => '===[' . date('d-m-Y H:i:s', $update['edited_message']['edit_date']) . ']===' . PHP_EOL,
+                array_key_exists('callback_query', $update) => '===[' . date('d-m-Y H:i:s', $update['callback_query']['message']['date']) . ']===' . PHP_EOL,
+                array_key_exists('open_weather', $update) => '===[' . date('d-m-Y H:i:s') . ']===' . PHP_EOL,
+                default => '===[' . date('d-m-Y H:i:s', $update['date']) . ']===' . PHP_EOL,
+            };
 
             print_r($update);
             echo '------------------' . PHP_EOL;
@@ -139,5 +145,106 @@ class TelegramBotApiHelper
             'chatId' => $chatId,
             'incomingText' => $incomingText,
         ];
+    }
+
+    /**
+     * @throws GuzzleException
+     */
+    private static function getResponseToOpenWeatherApi(
+        string $method,
+        string $city
+    ): string {
+        if (str_contains($city, ',')) {
+            $cityWithCode = explode(',', $city);
+            [$nameCity, $lang] = $cityWithCode;
+        } else {
+            $nameCity = $city;
+            $lang = 'ru';
+        }
+
+        $response = (new Client())->request(
+            $method,
+            OPEN_WEATHER_MAP_URL,
+            [
+                'query' => [
+                    'appid' => OPEN_WEATHER_MAP_TOKEN,
+                    'q' => trim($nameCity),
+                    'units' => 'metric',
+                    'lang' => trim($lang),
+                ],
+                'http_errors' => false
+            ]
+        );
+
+        $status = $response->getStatusCode();
+        $stream = $response->getBody();
+        $jsonResponse = $stream->getContents();
+        $decodeResponse = json_decode($jsonResponse, true);
+
+        switch ($status) {
+            case 401:
+                self::writeToLogs(
+                    [
+                        'open_weather' => [
+                            'cod' => $decodeResponse['cod'],
+                            'message' => $decodeResponse['message'],
+                        ]
+                    ],
+                    __DIR__ . '/../open-weather-api-error.txt'
+                );
+                $responseTxt = 'Проблема с доступом к сервису "Open Weather"';
+                break;
+            case 404:
+                self::writeToLogs(
+                    [
+                        'open_weather' => [
+                            'user_city_request' => $city,
+                            'cod' => $decodeResponse['cod'],
+                            'message' => $decodeResponse['message'],
+                        ]
+                    ],
+                    __DIR__ . '/../open-weather-api-error.txt'
+                );
+                $responseTxt = 'Город не найден';
+                break;
+            case 200:
+                self::writeToLogs(
+                    [
+                        'open_weather' => [
+                            'user_city_request' => $city,
+                            'response' => $decodeResponse,
+                        ]
+                    ],
+                    __DIR__ . '/../open-weather-api-response.txt'
+                );
+                //$responseTxt = '<pre>'.print_r($decodeResponse, true).'</pre>';
+                $responseTxt = <<< OPEN_WEATHER_RESPONSE
+                Город <code>{$decodeResponse['name']}</code> найден 👍
+                    🔸️ ID в сервисе OpenWeather: <code>{$decodeResponse['id']}</code>
+                    🔸️ Координаты: <code>{$decodeResponse['coord']['lon']}, {$decodeResponse['coord']['lat']}</code>
+
+                Информация о погоде:
+                    🔸️ ...
+                    🔸️ ...
+                    🔸️ ...
+                OPEN_WEATHER_RESPONSE;
+                break;
+            default:
+                self::writeToLogs(
+                    [
+                        'open_weather' => [
+                            'error' => 'Неизвестная ошибка',
+                            'status' => $status,
+                            'user_city_request' => $city,
+                            'decode_response' => $decodeResponse,
+                        ]
+                    ],
+                    __DIR__ . '/../open-weather-api-error.txt'
+                );
+                $responseTxt = 'Бот временно не доступен, повторите попытку позднее';
+                break;
+        }
+
+        return $responseTxt;
     }
 }
